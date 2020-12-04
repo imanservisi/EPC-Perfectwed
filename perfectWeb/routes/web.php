8ae0fb69469ff56203c15d27522b2bcb0ff74f72
Yann Fontaine <yann@localhost.localdomain>
2020-12-04 20:18:22 +0100

    Added routes for steps

--- perfectWeb/routes/web.php
+++ perfectWeb/routes/web.php
@@ -41,3 +41,35 @@ Route::post('calculation', [App\Http\Controllers\CalculatorController::class, 'i
 Route::get('/step1', function () {
     return Inertia\Inertia::render('step1');
 })->name('step1');
+
+Route::get('/step2', function () {
+    return Inertia\Inertia::render('step2');
+})->name('step2');
+
+Route::get('/step3', function () {
+    return Inertia\Inertia::render('step3');
+})->name('step3');
+
+Route::get('/step4', function () {
+    return Inertia\Inertia::render('step4');
+})->name('step4');
+
+Route::get('/step5', function () {
+    return Inertia\Inertia::render('step5');
+})->name('step5');
+
+Route::get('/step6', function () {
+    return Inertia\Inertia::render('step6');
+})->name('step6');
+
+Route::get('/step7', function () {
+    return Inertia\Inertia::render('step7');
+})->name('step7');
+
+Route::get('/step8', function () {
+    return Inertia\Inertia::render('step8');
+})->name('step8');
+
+Route::get('/step9', function () {
+    return Inertia\Inertia::render('step9');
+})->name('step9');
